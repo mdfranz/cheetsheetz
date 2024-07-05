dd4adff9765764f25fb00edeeffbc1ab0397bd5d
Matt Franz <mdfranz@gmail.com>
2024-07-04 20:50:20 -0400

    Unecessary

--- aws/eks/cluster-role.sh
+++
@@ -1,8 +0,0 @@
-aws iam create-role \
-  --role-name eksClusterRole \
-    --assume-role-policy-document file://"cluster-trust-policy.json"
-
-
-aws iam attach-role-policy \
-      --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy \
-        --role-name eksClusterRole
